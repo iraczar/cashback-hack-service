1f5a14c234d6ad61f8815e29dc1f678bcaf9529d
Ira.czar <ira.czar.02@bk.ru>
2026-01-10 15:54:25 +0300

    commit testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNGTest.java
@@ -0,0 +1,73 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTestNGTest {
+
+    @Test
+    public void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn999IfAmountIs1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs2000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn500IfAmountIs1500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+}
