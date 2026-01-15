505ef03656b978c8725101d075889478766ea97b
Ira.czar <ira.czar.02@bk.ru>
2026-01-15 18:53:28 +0300

    commit testng1

--- src/test/java/ru/netology/service/CashbackHackServiceTestNGTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNGTest.java
@@ -1,29 +1,25 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+import org.testng.annotations.Test; // Важно: импорт от TestNG!
+import static org.testng.Assert.*;   // Важно: импорт от TestNG!
 
-public class CashbackHackServiceTestNGTest {
+public class CashbackHackServiceTest {
 
     @Test
     public void shouldReturn100IfAmountIs900() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
-
         int actual = service.remain(amount);
         int expected = 100;
-
-        assertEquals(actual, expected);
+        assertEquals(actual, expected); // Порядок аргументов: actual, expected
     }
 
     @Test
     public void shouldReturn0IfAmountIs1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-
         int actual = service.remain(amount);
         int expected = 0;
-
         assertEquals(actual, expected);
     }
 
@@ -31,10 +27,8 @@ public class CashbackHackServiceTestNGTest {
     public void shouldReturn999IfAmountIs1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1;
-
         int actual = service.remain(amount);
         int expected = 999;
-
         assertEquals(actual, expected);
     }
 
@@ -42,10 +36,8 @@ public class CashbackHackServiceTestNGTest {
     public void shouldReturn0IfAmountIs2000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 2000;
-
         int actual = service.remain(amount);
         int expected = 0;
-
         assertEquals(actual, expected);
     }
 
@@ -53,21 +45,18 @@ public class CashbackHackServiceTestNGTest {
     public void shouldReturn500IfAmountIs1500() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1500;
-
         int actual = service.remain(amount);
         int expected = 500;
-
         assertEquals(actual, expected);
     }
 
+    // Тест, который выявит баг
     @Test
     public void shouldReturn0IfAmountIs0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
-
         int actual = service.remain(amount);
         int expected = 0;
-
-        assertEquals(actual, expected);
+        assertEquals(actual, expected); // Этот тест упадёт, т.к. метод вернёт 1000
     }
 }
